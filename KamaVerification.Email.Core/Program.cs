22d599929d205f9cb3332ef14aadb37e7d7a4941
Georgi Kamacharov <gkamacharov@gmail.com>
2022-03-20 09:55:32 -0400

    add sendgrid client

--- KamaVerification.Email.Core/Program.cs
+++ KamaVerification.Email.Core/Program.cs
@@ -1,11 +1,17 @@
 using KamaVerification.Services;
 using KamaVerification.Email.Services;
+using SendGrid;
+using SendGrid.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+var config = builder.Configuration;
 
-services.AddFluentEmail("localhost@kamaverification")
-    .AddSmtpSender("smtp.example.com", 587);
+services.AddScoped<IEmailVerificationRepository, EmailVerificationRepository>()
+    .AddSendGrid(o =>
+    {
+        o.ApiKey = config["SendGrid:ApiKey"];
+    });
 
 services.AddScoped<IVerificationRepository, VerificationRepository>()
     .AddScoped<IEmailVerificationRepository, EmailVerificationRepository>()
--- KamaVerification.Email.Services/VerificationRepository.Email.cs
+++ KamaVerification.Email.Services/VerificationRepository.Email.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
-using KamaVerification.Services;
-using FluentEmail.Core;
+﻿using KamaVerification.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace KamaVerification.Email.Services
 {
@@ -12,30 +14,38 @@ namespace KamaVerification.Email.Services
     public class EmailVerificationRepository : IEmailVerificationRepository
     {
         private readonly ILogger<EmailVerificationRepository> _logger;
+        private readonly IConfiguration _config;
         private readonly IVerificationRepository _repo;
-        private readonly IFluentEmail _fluentEmail;
+        private readonly ISendGridClient _sendGridClient;
 
         public EmailVerificationRepository(
             ILogger<EmailVerificationRepository> logger,
-            IVerificationRepository repo, 
-            IFluentEmail fluentEmail)
+            IConfiguration config,
+            IVerificationRepository repo,
+            ISendGridClient sendGridClient)
         {
             _logger = logger;
+            _config = config;
             _repo = repo;
-            _fluentEmail = fluentEmail;
+            _sendGridClient = sendGridClient;
 
         }
 
         public async Task SendAsync()
         {
-            var email = _fluentEmail
-                .SetFrom("bill.gates@microsoft.com")
-                .To("gkamacharov@gmail.com", "Georgi")
-                .Subject("Hi Georgi!")
-                .Body("Fluent email looks great!");
-            var emailResponse = await email.SendAsync();
+            var msg = new SendGridMessage
+            {
+                From = new EmailAddress(_config["SendGrid:From"], "Georgi"),
+                Subject = "Sending with SendGrid is Fun"
+            };
+            msg.AddContent(MimeType.Html, "<strong>and easy to do anywhere, even with C#</strong>");
+            msg.AddContent(MimeType.Text, "and easy to do anywhere, even with C#");
+            msg.AddTo(new EmailAddress("yifet83692@sofrge.com", "Example User"));
 
-            if (emailResponse.Successful) _logger.LogInformation("Successfully sent email");
+            var response = await _sendGridClient.SendEmailAsync(msg);
+
+            if (response.IsSuccessStatusCode) _logger.LogInformation("Successfully sent email");
+            else throw new Exception(string.Join(", ", response.Body));
         }
     }
 }
