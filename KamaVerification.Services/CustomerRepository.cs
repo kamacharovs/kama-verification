91a78e9ac0a642c75fccda831aafbb45522138d1
Georgi Kamacharov <gkamacharov@gmail.com>
2022-03-23 15:00:15 -0400

    add get and delete async methods

--- KamaVerification.Services/CustomerRepository.cs
+++ KamaVerification.Services/CustomerRepository.cs
@@ -3,12 +3,15 @@ using KamaVerification.Data.Models;
 using KamaVerification.Data.Dtos;
 using Microsoft.Extensions.Logging;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace KamaVerification.Services
 {
     public interface ICustomerRepository
     {
+        Task<Customer> GetAsync(int customerId);
         Task<Customer> AddAsync(CustomerDto dto);
+        Task<bool> DeleteAsync(int customerId);
     }
 
     public class CustomerRepository : ICustomerRepository
@@ -27,6 +30,12 @@ namespace KamaVerification.Services
             _context = context;
         }
 
+        public async Task<Customer> GetAsync(int customerId)
+        {
+            return await _context.Customers
+                .FirstOrDefaultAsync(x => x.CustomerId == customerId);
+        }
+
         public async Task<Customer> AddAsync(CustomerDto dto)
         {
             var customer = _mapper.Map<Customer>(dto);
@@ -34,7 +43,24 @@ namespace KamaVerification.Services
             await _context.Customers.AddAsync(customer);
             await _context.SaveChangesAsync();
 
+            _logger.LogInformation("Added customer with name={CustomerName}",
+                customer.Name);
+
             return customer;
+        }
+
+        public async Task<bool> DeleteAsync(int customerId)
+        {
+            var customer = await GetAsync(customerId);
+            customer.IsDeleted = true;
+
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("Deleted customer with name={CustomerName}",
+                customer.Name);
+
+            return true;
         }
     }
 }
