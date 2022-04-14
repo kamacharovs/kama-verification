390a1ef4bfc3b9dbc2b9ac329890dc9b7a25f941
Georgi Kamacharov <gkamacharov@gmail.com>
2022-04-14 11:00:24 -0400

    feature/22/04/add-terraform (#5)
    * update dockerfile migration and add terraform pipeline
    
    * update pipeline and add tf code
    
    * update
    
    * update main and workdir
    
    * update format
    
    * add cli config credentials
    
    * update to remove needs: terraform

---
+++ Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "azure_subscription_id" {
+  type        = string
+  description = "Azure subscription id"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "Azure tenant id"
+}
+
+variable "azure_client_id" {
+  type        = string
+  description = "Azure service principal (app registration) client id"
+}
+
+variable "azure_client_secret" {
+  type        = string
+  description = "Azure service principal (app registration) client secret"
+}
