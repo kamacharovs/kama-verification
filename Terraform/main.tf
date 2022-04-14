aa1d11e9aecfa597d791381c725e246a2c582377
Georgi Kamacharov <gkamacharov@gmail.com>
2022-04-14 12:05:00 -0400

    add max_retries = 1

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -25,6 +25,7 @@ provider "azurerm" {
   tenant_id       = var.azure_tenant_id
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
+  max_retries     = 1
 
   features {}
 }
